f5b8f34aaef25d1cd6a6139fd6d1cf9517bd667c
silvia <silviadesiderii01@gmail.com>
2025-04-17 13:19:36 +0200

    modifiche

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,8 +1,10 @@
 const Sidebar = () => {
   return (
-    <div>
-      
-      <a href="http://">Cos'è la discalculia</a>
+    <div className="display grid gap-3.5">
+      <img src="C:\Users\Silvia\OneDrive\Desktop\FS-35-TeamProject2\FS35-Team1-Project2\immagini\logo.svg" alt="" />
+      <a href="http://">Cos'è la discalculia</a> 
+      <a href="http://">Quiz</a> 
+      <a href="http://">Feedback</a>
     </div>
   );
 };
