d0ebf1888f0d76092ac0ec7a60d3c6d0a171f685
silvia <silviadesiderii01@gmail.com>
2025-04-17 13:46:42 +0200

    modifiche

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,14 @@
 const Sidebar = () => {
   return (
-    <div className="display grid gap-3.5">
-      <img src="C:\Users\Silvia\OneDrive\Desktop\FS-35-TeamProject2\FS35-Team1-Project2\immagini\logo.svg" alt="" />
-      <a href="http://">Cos'è la discalculia</a> 
-      <a href="http://">Quiz</a> 
-      <a href="http://">Feedback</a>
+    <div className="flex flex-col space-y-4 p-4 w-full max-w-xs">
+      <div className="flex justify-center">
+        
+        <img className="w-[111px]" src="immagini/logo.svg" alt="Logo" />
+      </div>
+
+      <div><a href="http://">Cos'è la discalculia</a></div>
+      <div><a href="http://">Quiz</a></div>
+      <div><a href="http://">Feedback</a></div>
     </div>
   );
 };
