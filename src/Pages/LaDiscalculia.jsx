d165d663cff4cedcc427f082c690de87957fe8b2
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-17 20:05:57 +0200

    sezione cos'è la discalculia creata

---
+++ src/Pages/LaDiscalculia.jsx
@@ -0,0 +1,46 @@
+import SezioniLaDiscalculia from "../Components/SezioniLaDiscalculia"
+import Footer from "../Components/Footer"
+
+
+
+const LaDiscalculia= ()=>{
+
+    return (
+        <>
+<nav className="flex justify-center bg-amber-950 p-4 mb-10" >
+  <img src="immagini/logo.svg" alt="" className="w-20"/>
+</nav>
+
+<SezioniLaDiscalculia
+srcImg= "immagini/bambino dubbioso.svg"  
+bg="00AD68" 
+  title="Cos'è la discalculia?" 
+  text="La discalculia è un disturbo specifico dell’apprendimento che riguarda la matematica. Chi ne soffre può avere difficoltà a comprendere i numeri, fare calcoli o riconoscere quantità e simboli matematici, anche se ha intelligenza nella norma."
+/>
+<SezioniLaDiscalculia 
+ bg="FB8F20" 
+ srcImg= "immagini/bambina libri.svg"  
+  title="Cause della discalculia" 
+  text="Le cause non sono ancora del tutto chiare, ma si pensa che siano legate al modo in cui il cervello elabora le informazioni numeriche. La discalculia non dipende da pigrizia o mancanza di impegno, ed è spesso ereditaria."
+  reverseColumns={true}
+/>  
+<SezioniLaDiscalculia 
+ bg="EFD45A" 
+ srcImg= "immagini/bambina dubbiosa.svg"  
+  title="Segnali della discalculia" 
+  text="I segnali possono variare, ma tra i più comuni ci sono: difficoltà nel contare, confusione tra numeri simili, problemi con le tabelline, o nel capire il concetto di tempo e misura. Spesso si notano già nei primi anni di scuola."
+  reverse={true}
+/>      
+<SezioniLaDiscalculia 
+ bg="5C9BFF" 
+ srcImg= "immagini/bambino.svg"  
+  title="Soluzioni pratiche" 
+  text="Con il giusto supporto, si può imparare la matematica in modo diverso! Strumenti visivi, attività tattili, giochi interattivi e strategie su misura possono fare una grande differenza nell’apprendimento."
+  lastReverse={true}
+/>  
+<Footer/>
+
+        </>
+    )
+}
+export default LaDiscalculia
