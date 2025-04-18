9a758c194818c119b38a7b12d450c561535b72c7
silvia <silviadesiderii01@gmail.com>
2025-04-18 10:11:48 +0200

    aggiunte modifiche

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "../Components/HeroSection";
 import CallToAction from "../Components/CallToAction";
 import Footer from "../Components/Footer";
+import Sidebar from "../Components/Sidebar";
 
 const HomePage = () => {
   return (
