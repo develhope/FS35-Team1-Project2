f10aa879ceff26788413787f6e360913aab4ae87
Morena D'Asta <morena.work.0907@gmail.com>
2025-04-18 10:04:24 +0200

    completato header project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,17 @@
-const Header = ()=>{
-    return
+const Header = () => {
+  return (
+    <div className="flex justify-between w-full mt-2">
+      <img src="./immagini/icon/hamburger.svg" className="w-[24px] ml-1" />
+      <img src="./immagini/logo.svg" className="w-[80px] ml-4" />
+      <div className="flex ">
+        <img src="./immagini/icon/profile.svg" className="w-[20px]" />
+        <img
+          src="./immagini/icon/usp-delivery-store.svg"
+          className="w-[30px] ml-2 "
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,15 @@
 import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
 
 
 const HomePage = () => {
 
     return (
+        <div>
+        <Header/>
         <Footer />
+        </div>
     )
 };
 
