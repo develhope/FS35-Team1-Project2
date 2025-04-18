d6fdfc333513d493019e9c548070bdb9ca0f2469
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-18 12:22:34 +0200

    add import nell'home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import HeroSection from "../Components/HeroSection";
+import CallToAction from "../Components/CallToAction";
 
 const HomePage = () => {
   return (
     <>
+
       <Header />
       <HeroSection />
       <CallToAction text="Conosciamoci meglio!" />
