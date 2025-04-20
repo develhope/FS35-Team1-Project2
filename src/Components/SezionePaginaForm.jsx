049599bf1d7a1e126b0fa27e38cad4fee2a07511
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-04-20 15:50:23 +0200

    add branch pagine form

---
+++ src/Components/SezionePaginaForm.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+const SezionePaginaForm = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default SezionePaginaForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import HomePage from './Pages/HomePage'
-import LaDiscalculia from './Pages/LaDiscalculia'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import HomePage from "./Pages/HomePage";
+import LaDiscalculia from "./Pages/LaDiscalculia";
+import PaginaForm from "./Components/SezionePaginaForm";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LaDiscalculia  /> 
-   <HomePage />
-  </StrictMode>,
-)
+    <PaginaForm />
+  </StrictMode>
+);
