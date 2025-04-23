52e632a9e51f49f355d0bfd7f53582b2bf50201f
Alfonso Lambiase <lambiase1199@gmail.com>
2025-04-23 16:25:13 +0200

    add demo form

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import HomePage from "./Pages/HomePage";
-import LaDiscalculia from "./Pages/LaDiscalculia";
-import Profile from "./Pages/Profile";
+import Form from "./Pages/Form";
+import FormIscriviti from "./Pages/FormIscriviti";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HomePage />
-    <Profile />
-    <LaDiscalculia />
+    <Form/>
+    <FormIscriviti/>
   </StrictMode>
 );
