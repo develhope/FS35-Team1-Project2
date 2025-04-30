d1502dd2b8a5989677a1335885415e2d60d491b2
silvia <silviadesiderii01@gmail.com>
2025-04-30 11:40:39 +0200

    fatto shop

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,6 +2,7 @@ import HeroSection from "../Components/HeroSection";
 import CallToAction from "../Components/CallToAction";
 import Footer from "../Components/Footer";
 import Sidebar from "../Components/Sidebar";
+import Shop from "./Shop";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <HeroSection />
       <Sidebar/>
       <CallToAction text="Conosciamoci meglio!" />
+      <Shop/>
       <Footer />
       
     </>
