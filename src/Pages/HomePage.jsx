ddc85d3334da245e44e66caa976dc5c3dc5cfe91
Morena D'Asta <morena.work.0907@gmail.com>
2025-04-30 12:20:02 +0200

    sistemazione componente header

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,7 +7,15 @@ import GameSection from "../Components/GameSection";
 const HomePage = () => {
   return (
     <>
-      <Header leftIcon={<img src="./immagini/icon/hamburger.svg" className="w-[24px] ml-1" />} centerIcon={<img src="./immagini/logo.svg" className="w-[80px] ml-5" />}/>
+      <Header
+        leftIcon={
+          <img src="./immagini/icon/hamburger.svg" className="w-[24px] ml-1" />
+        }
+        centerIcon={<img src="./immagini/logo.svg" className="w-[80px] ml-6" />}
+        profileIcon={
+          <img src="../../immagini/icon/profile.svg" className="w-[20px]" />
+        }
+      />
       <HeroSection />
       <GameSection />
       <CallToAction text="Conosciamoci meglio!" />
