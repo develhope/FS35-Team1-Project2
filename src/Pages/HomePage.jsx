ea135b6a0007a921607e349cf34049037b057853
Mattia Lucia <mattia.lucia.00@gmail.com>
2025-05-12 19:58:51 +0200

    add: Router e modifiche pulsanti header e sidebar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,27 +1,24 @@
 import Footer from "../Components/Footer";
-import Sidebar from "../Components/Sidebar";
 import CallToAction from "../Components/CallToAction";
 import HeroSection from "../Components/HeroSection";
 import Header from "../Components/Header";
 import GameSection from "../Components/GameSection";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <Header
         leftIcon={
-          <img src="./immagini/icon/hamburger.svg" className="w-[24px] ml-1" />
+          <Link to="/sidebar"><img src="./immagini/icon/hamburger.svg" className="w-[24px] ml-1"/></Link>
         }
         centerIcon={<img src="./immagini/logo.svg" className="w-[80px] ml-6" />}
       />
       <div>
         <div>
           <HeroSection />
-
-          <Sidebar />
           <GameSection />
           <CallToAction text="Conosciamoci meglio!" />
-
           <Footer />
         </div>
       </div>
---
+++ src/app.jsx
@@ -0,0 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import HomePage from "./Pages/HomePage";
+import Form from "./Pages/Form";
+import FormIscriviti from "./Pages/FormIscriviti";
+import Quiz from "./Pages/Quiz";
+import LaDiscalculia from "./Pages/LaDiscalculia";
+import Profile from "./Pages/Profile";
+import Shop from "./Pages/Shop";
+import GamePage1 from "./Pages/GamePage1";
+import GamePage2 from "./Pages/GamePage2";
+import GamePage3 from "./Pages/GamePage3";
+import GamePage4 from "./Pages/GamePage4";
+import AnteprimaGioco1 from "./Pages/AnteprimaGioco1";
+import AnteprimaGioco2 from "./Pages/AnteprimaGioco2";
+import AnteprimaGioco3 from "./Pages/AnteprimaGioco3";
+import AnteprimaGioco4 from "./Pages/AnteprimaGioco4";
+import Sidebar from "./Components/Sidebar";
+
+function App() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/quiz" element={<Quiz />} />
+                <Route path="/gamepage-1" element={<GamePage1 />} />
+                <Route path="/gamepage-2" element={<GamePage2 />} />
+                <Route path="/gamepage-3" element={<GamePage3 />} />
+                <Route path="/gamepage-4" element={<GamePage4 />} />
+                <Route path="/form-iscriviti" element={<FormIscriviti />} />
+                <Route path="/la-discalculia" element={<LaDiscalculia />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/anteprimagioco1" element={<AnteprimaGioco1/>} />
+                <Route path="/anteprimagioco2" element={<AnteprimaGioco2/>} />
+                <Route path="/anteprimagioco3" element={<AnteprimaGioco3/>} />
+                <Route path="/anteprimagioco4" element={<AnteprimaGioco4/>} />
+                <Route path="/sidebar" element={<Sidebar/>} />
+            </Routes>
+        </>
+    )
+}
+
+export default App
