589bd200dbe029125c5dc5298cc328a7f0f5bddc
Alfonso Lambiase <lambiase1199@gmail.com>
2025-05-22 15:10:26 +0200

    component star

--- src/Giochi/Gioco4/Struttura4Gioco.jsx
+++ src/Giochi/Gioco4/Struttura4Gioco.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Star from "../../Components/Star";
 
 const Struttura4Gioco = () => {
   const [showError, setShowError] = useState(false);
@@ -93,44 +94,7 @@ const Struttura4Gioco = () => {
         {showSuccess && (
   <>
     
-
-   <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-      <div
-        style={{
-          position: "relative",
-          fontSize: "400px",       
-          color: "yellow",
-          userSelect: "none",
-          lineHeight: 1,
-          width: "320px",
-          height: "320px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          textShadow: "2px 2px 4px rgba(0,0,0,0.3)",
-        }}
-      >
-        ★
-        <span
-          style={{
-            position: "absolute",
-            top: "60%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            color: "white",
-            fontWeight: "700",
-            fontSize: "20px",       // scritta più piccola
-            textAlign: "center",
-            width: "80%",
-            userSelect: "none",
-            pointerEvents: "none",
-          }}
-        >
-          Risposta <br/> esatta!
-        </span>
-      </div>
-    </div>
-
+   <Star/>
 
     <button
       className="absolute bottom-3 right-3 bg-orange-600 text-white px-3 py-1 rounded shadow "
