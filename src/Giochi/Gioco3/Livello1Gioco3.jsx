541216fed85afce537c7a5a890f806acba46e4fc
silvia <silviadesiderii01@gmail.com>
2025-05-22 20:23:00 +0200

    livello1-2 gioco3

--- src/Giochi/Gioco3/Livello1Gioco3.jsx
+++ src/Giochi/Gioco3/Livello1Gioco3.jsx
@@ -1,29 +1,35 @@
+import React from "react";
 import Struttura3Gioco from "./Struttura3Gioco";
 
-
 const Livello1Gioco3 = () => {
+  const blocchiPerLivello1 = [
+    { tipo: 'numero', valore: "2", colore: 'pink' },
+    { tipo: 'frutto', valore: "tre banane", colore: 'lilac' },
+    { tipo: 'frutto', valore: "due mele", colore: 'blue' },
+    { tipo: 'numero', valore: "3", colore: 'yellow' },
+  ];
+
+  // Calcola la larghezza della griglia basandoti sul numero di colonne e il gap
+  // Larghezza di un blocco: w-20 = 80px
+  // gap-x-0 = 0px
+  // 2 colonne * 80px/colonna + 0px di gap = 160px
+  const gridCalcWidth = 'w-[160px]'; // Per 2 colonne w-20 e gap-x-0
+
   return (
-    
     <Struttura3Gioco
-    
-     
-      traccia="Quante mele ci sono?"
-             sottotraccia="Aiutati contando con le dita"
-//       imgs={[
-//         "https://i.etsystatic.com/15835007/r/il/ffcf81/3319408935/il_570xN.3319408935_5a1a.jpg",
-//         "https://i.etsystatic.com/15835007/r/il/ffcf81/3319408935/il_570xN.3319408935_5a1a.jpg",
-//         "https://i.etsystatic.com/15835007/r/il/ffcf81/3319408935/il_570xN.3319408935_5a1a.jpg"
-//       ]}
-     opz1="2"
-      opz2="5"
-             opz3="3"
-      rispostaCorretta="3"
-nebula="../../immagini/Gioco1/nebula giochi.png"
-      prossimoLivelloLink="/livello2"
-     posizioneAstronauti={{
-  donna: { left: 60, top: 60 },
-  maschio: { left: 230, top: 20 },
-}}
+      titoloLivello="Unisci i frutti con la quantità numerica giusta!"
+      istruzioniTesto="Trascina il numero giusto sull’immagine giusta o viceversa."
+      risposteCorrette={{
+        "2": "due mele",
+        "3": "tre banane",
+      }}
+      livelloSuccessivoPath="/livello2gioco3"
+      blocchiGioco={blocchiPerLivello1}
+      // NUOVE PROPS PER LA GRIGLIA
+      gridCols="grid-cols-2" // 2 colonne
+      gapX="gap-x-5"        // Nessun gap orizzontale
+      gapY="gap-y-6"        // Gap verticale di 16px (4 unità Tailwind)
+      gridWidth={gridCalcWidth} // Larghezza calcolata per 2 colonne e 0 gap
     />
   );
 };
