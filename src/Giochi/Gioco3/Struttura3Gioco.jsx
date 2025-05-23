f6a697066ed05ed55190eb3969cb13bc05838baa
silvia <silviadesiderii01@gmail.com>
2025-05-23 12:51:50 +0200

    aggiunto componente stella

--- src/Giochi/Gioco3/Struttura3Gioco.jsx
+++ src/Giochi/Gioco3/Struttura3Gioco.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Star from "../../Components/Star";
 
 const Struttura3Gioco = ({
   titoloLivello,
@@ -130,42 +131,7 @@ const Struttura3Gioco = ({
 
       {rispostaEsatta && (
         <>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-            <div
-              style={{
-                position: "relative",
-                fontSize: "400px",
-                color: "yellow",
-                userSelect: "none",
-                lineHeight: 1,
-                width: "320px",
-                height: "320px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                textShadow: "2px 2px 4px rgba(0,0,0,0.3)",
-              }}
-            >
-              ★
-              <span
-                style={{
-                  position: "absolute",
-                  top: "60%",
-                  left: "50%",
-                  transform: "translate(-50%, -50%)",
-                  color: "white",
-                  fontWeight: "700",
-                  fontSize: "20px",
-                  textAlign: "center",
-                  width: "80%",
-                  userSelect: "none",
-                  pointerEvents: "none",
-                }}
-              >
-                Risposta <br /> esatta!
-              </span>
-            </div>
-          </div>
+         <Star/>
           <p className="absolute bottom-3 left-3 text-white">
             Hai raccolto <span className="text-yellow-300">100</span> punti
           </p>
