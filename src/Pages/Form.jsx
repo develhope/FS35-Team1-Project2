d03102e8cea6ed3e9f06e7ed40507724391e9985
Cristian Rinaldi <cristianrinaldi0@gmail.com>
2025-06-04 20:39:58 +0200

    modificata pagina quiz form

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -7,16 +7,16 @@ const Form = () => {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center py-[50px] space-y-7 text-center mt-10">
-        <h1 className="text-2xl">Parliamo un pò</h1>
-        <h2 className="text-[18px]">Prima di partire... facciamo squadra!</h2>
-        <h3 className="text-[14px] px-[56px]">
+      <div className="flex flex-col justify-center items-center py-[50px] space-y-7 text-center mt-10 md:mt-[100px]">
+        <h1 className="text-2xl md:text-[36px]" >Parliamo un pò</h1>
+        <h2 className="text-[18px] md:text-[24px]">Prima di partire... facciamo squadra!</h2>
+        <h3 className="text-[14px] px-[56px] md:text-[24px]">
           Il quiz serve a conoscerti meglio e ci piace sapere chi ci sta
           aiutando. <br /> Sei il genitore?
         </h3>
       </div>
 
-      <div className="flex flex-row justify-between items-start p-5 space-x-10">
+      <div className="flex flex-row justify-between items-start p-5 space-x-10 md:text-[34px]">
         <div className="flex flex-col justify-start items-start space-y-7 text-left w-fit relative">
           <form className="space-y-5">
             <label className="flex items-center space-x-5">
@@ -54,7 +54,7 @@ const Form = () => {
           </form>
 
           {scelta === "bambino" && (
-            <div className="absolute top-full mt-2 left-0 bg-[#292929]/90 text-white rounded-2xl px-6 py-4 text-sm shadow-lg max-w-md z-50 backdrop-blur-sm">
+            <div className="absolute top-full mt-2 left-0 bg-[#292929]/90 text-white rounded-2xl px-6 py-4 text-sm md:text-2xl shadow-lg max-w-md z-50 backdrop-blur-sm">
               Il quiz è pensato per essere fatto con l’aiuto di un adulto. 💡
               <br />
               Corri a dirlo a mamma e papà!
@@ -71,14 +71,15 @@ const Form = () => {
             </Link>
           </div>
         </div>
+<div className="fixed bottom-20 right-20 translate-x-1/2 md:bottom-10 md:right-14 md:translate-x-0 z-10">
+  <img
+    src="./immagini/nebula_primaForm.png"
+    alt="NebulaForm"
+    className="w-[120px] md:w-[230px] object-contain"
+  />
+</div>
 
-        <div className="w-fit fixed bottom-20 right-6">
-          <img
-            src="./immagini/nebula_primaForm.png"
-            alt="NebulaForm"
-            className="h-70 object-contain"
-          />
-        </div>
+
       </div>
     </>
   );
